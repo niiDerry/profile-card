baa0ae436395c518028f0eb0e13fdba88dd3f505
Nii Derry <derricktagoe@gmail.com>
2024-02-01 16:38:40 +0000

    challenge 1 profile card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <div>
+      <img className="avatar" src="./img/IMG_9343ff.PNG" alt="avatar" />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Derry Angelo Tagoe</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum
+        molestias illo iure iste maiores excepturi aliquid, eveniet voluptate?
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skillName="HTML + CSS" emoji="💪" />
+      <Skill skillName="Web Design" emoji="👍" />
+      <Skill skillName="JavaScript" emoji="😊" />
+      <Skill skillName="React" emoji="👶" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill">
+      <p>
+        {props.skillName} {props.emoji}
+      </p>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
